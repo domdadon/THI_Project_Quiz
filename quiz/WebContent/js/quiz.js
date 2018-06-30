b746dfbb299e6b879ffa2eeca7bc72a0aa53d6c8
domdadon <dom4812@thi.de>
2018-06-30 13:12:34 +0200

    fix js with event

--- quiz/WebContent/js/quiz.js
+++ quiz/WebContent/js/quiz.js
@@ -3,30 +3,30 @@ document.addEventListener("DOMContentLoaded", init);
 var element;
 var xmlhttp = null;
 function init() {
-	document.getElementById("answer1").addEventListener("click", checkAnswer);
+	document.getElementById("answer1").addEventListener("click", function(){checkAnswer(event);});
 	document.getElementById("answer2").addEventListener("click", checkAnswer);
 	document.getElementById("answer3").addEventListener("click", checkAnswer);
 	document.getElementById("answer4").addEventListener("click", checkAnswer);
 }
 
 
-function checkAnswer() {
-	//var source = event.target.id;
-	var source = document.getElementById("a_ID1").id;
+function checkAnswer(event) {
+	var source = event.target.id
+	//var source = document.getElementById("a_ID1").id;
 	var q_ID = document.getElementById("q_ID").value;
 	var a_ID;
 	
-	if (source == "a_ID1") {
+	if (source == "answer1") {
 			a_ID = document.getElementById("a_ID1").value;
-	} else if (source == "a_ID2") {
+	} else if (source == "answer2") {
 			a_ID = document.getElementById("a_ID2").value;
-	} else if (source == "a_ID3") {
+	} else if (source == "answer3") {
 			a_ID = document.getElementById("a_ID3").value;
-	} else if (source == "a_ID4") {
+	} else if (source == "answer4") {
 			a_ID = document.getElementById("a_ID4").value;
 	}
 	
-	//element = event.target;
+	element = event.target;
 	
 	var par = "action=setAnswer&question="+q_ID+"&answer="+a_ID
 	element = document.getElementById("a_ID1");
